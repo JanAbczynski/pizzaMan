39d301f7e1079e72b1c9db834d8b1eac1da385d1
JanAbczynski <jan.abczynski@gmail.com>
2020-01-07 23:56:54 +0100

    day 2 finished

--- RelevantPizza/Models/Customer.cs
+++ RelevantPizza/Models/Customer.cs
@@ -10,6 +10,7 @@ namespace RelevantPizza.Models
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
         public string StreetAddress { get; set; }
         public string City { get; set; }
         public string State { get; set; }
